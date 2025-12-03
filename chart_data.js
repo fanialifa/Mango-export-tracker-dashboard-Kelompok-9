6712b6b2aecbd03729ad430102d6478baed348f1
fanialifa <fanialifa@apps.ipb.ac.id>
2025-12-03 23:50:07 +0700

    Add files via upload

---
+++ chart_data.js
@@ -0,0 +1,57 @@
+// Auto-generated chart data from CSV
+const chartData = {
+  "status": {
+    "Delivered": 350,
+    "Harvested": 350,
+    "Quality Check": 334,
+    "Exported": 325,
+    "At Port": 321,
+    "In Transit": 320
+  },
+  "countries": {
+    "Singapore": 232,
+    "China": 231,
+    "Malaysia": 231,
+    "UAE": 227,
+    "Japan": 227,
+    "Hong Kong": 220,
+    "Thailand": 214,
+    "Saudi Arabia": 211,
+    "South Korea": 207
+  },
+  "varieties": {
+    "Podang": 350,
+    "Gedong Gincu": 347,
+    "Golek": 344,
+    "Arumanis": 338,
+    "Harum Manis": 325,
+    "Manalagi": 296
+  },
+  "grades": {
+    "B": 678,
+    "A": 662,
+    "C": 660
+  },
+  "provinces": {
+    "Jawa Tengah": 319,
+    "Bali": 291,
+    "Jawa Barat": 290,
+    "Sumatra Utara": 285,
+    "Jawa Timur": 281,
+    "NTB": 275,
+    "Lampung": 259
+  },
+  "monthly": {
+    "2023-12": 4,
+    "2024-01": 206,
+    "2024-02": 192,
+    "2024-03": 196,
+    "2024-04": 223,
+    "2024-05": 180,
+    "2024-06": 221,
+    "2024-07": 209,
+    "2024-08": 181,
+    "2024-09": 195,
+    "2024-10": 193
+  }
+};
